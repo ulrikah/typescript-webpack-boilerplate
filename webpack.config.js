65587d8790160980df5ac8057cf9a55665a5b278
ulrikah <ulrikhalmoy@gmail.com>
2020-08-08 17:13:56 +0200

    Source map

--- webpack.config.js
+++ webpack.config.js
@@ -3,14 +3,15 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
+    mode: 'development',
     entry: {
         app: './src/index'
     },
     output: {
         filename: '[name].bundle.js',
         path: path.resolve(__dirname, 'dist'),
-  },
-
+    },
+    devtool: 'inline-source-map',
     plugins: [
         new CleanWebpackPlugin(),
         new HtmlWebpackPlugin({
