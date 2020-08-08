bfa51e8afa5cd7e8a2cff5e682119dfa3a29893f
ulrikah <ulrikhalmoy@gmail.com>
2020-08-08 16:48:46 +0200

    Remove assets

--- src/index.js
+++ src/index.js
@@ -5,10 +5,6 @@ function component() {
   
     // Lodash, currently included via a script, is required for this line to work
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-    element.classList.add('hello');
-    const icon = new Image();
-    icon.src = Icon;
-    element.appendChild(icon);
   
     return element;
   }
